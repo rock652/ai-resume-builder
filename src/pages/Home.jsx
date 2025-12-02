171af6901227fa8b809f10179e00955f75c0ec09
Rahul Yadav <you@example.com>
2025-12-02 19:23:45 +0530

    creating home page section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Banner from "../components/home/Banner";
+import Hero from "../components/home/Hero";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Banner />
+      <Hero />
+    </div>
+  );
 };
 
 export default Home;
